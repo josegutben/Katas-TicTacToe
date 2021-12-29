1ce23b3e2b32bc8d468f399211d0f13511ad70fc
Jose Gutiérrez <jogutierrez@aidacanarias.com>
2021-12-29 07:43:37 +0000

    red: players_can_not_move_twice_in_the_same_position

--- TicTacToeKata/TicTacToe.Tests/GameShould.cs
+++ TicTacToeKata/TicTacToe.Tests/GameShould.cs
@@ -28,6 +28,14 @@ namespace TicTacToe.Tests {
             wrongPlayerTurn.Should().Throw<MovementCouldNotBeCompletedException>().And.Reason.Should().Be(MovementErrorReason.NoPlayerTurn);
         }
 
+        [Test]
+        public void players_can_not_move_twice_in_the_same_position() {
+            Play(new SymbolPlayer('X'), 0, 0);
+            Action wrongMovement = () => Play(new SymbolPlayer('O'), 0, 0);
+
+            wrongMovement.Should().Throw<MovementCouldNotBeCompletedException>().And.Reason.Should().Be(MovementErrorReason.PositionAlreadyInUse);
+        }
+
         private void Play(SymbolPlayer symbolPlayer, int x, int y) {
             game.Play(symbolPlayer, x, y);
         }
--- TicTacToeKata/TicTacToe/Exceptions/MovementCouldNotBeCompletedException.cs
+++ TicTacToeKata/TicTacToe/Exceptions/MovementCouldNotBeCompletedException.cs
@@ -11,6 +11,7 @@ namespace TicTacToe.Exceptions {
 
     public enum MovementErrorReason {
         WrongFirstPlayer,
-        NoPlayerTurn
+        NoPlayerTurn,
+        PositionAlreadyInUse
     }
 }
