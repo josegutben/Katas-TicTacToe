542635e9593716e60a4246a884fe93623267f251
Jose Gutiérrez <jogutierrez@aidacanarias.com>
2021-12-29 07:27:50 +0000

    green: players_can_not_move_twice

--- TicTacToeKata/TicTacToe/Game.cs
+++ TicTacToeKata/TicTacToe/Game.cs
@@ -2,20 +2,30 @@
 
 namespace TicTacToe {
     public class Game {
-        private readonly char lastSymbol;
+        private SymbolPlayer lastSymbol;
 
         public Game() {
-            lastSymbol = ' ';
+            lastSymbol = new SymbolPlayer(' ');
         }
 
         public void Play(SymbolPlayer symbolPlayer, int x, int y) {
             if (IsOFirstPlayer(symbolPlayer)) {
                 throw new MovementCouldNotBeCompletedException(MovementErrorReason.WrongFirstPlayer);
             }
+
+            if (IsNoTurnForPlayer(symbolPlayer)) {
+                throw new MovementCouldNotBeCompletedException(MovementErrorReason.NoPlayerTurn);
+            }
+
+            lastSymbol = symbolPlayer;
+        }
+
+        private bool IsNoTurnForPlayer(SymbolPlayer symbolPlayer) {
+            return lastSymbol.Equals(symbolPlayer);
         }
 
         private bool IsOFirstPlayer(SymbolPlayer symbolPlayer) {
-            return lastSymbol == ' ' && symbolPlayer.IsOPlayer();
+            return lastSymbol.IsEmpty() && symbolPlayer.IsOPlayer();
         }
     }
 }
--- TicTacToeKata/TicTacToe/SymbolPlayer.cs
+++ TicTacToeKata/TicTacToe/SymbolPlayer.cs
@@ -1,4 +1,7 @@
-﻿namespace TicTacToe {
+﻿using System;
+using System.Drawing;
+
+namespace TicTacToe {
     public class SymbolPlayer {
         private readonly char symbol;
 
@@ -8,6 +11,23 @@
 
         public bool IsOPlayer() {
             return symbol == 'O';
+        }
+
+        public bool IsEmpty() {
+            return symbol == ' ';
+        }
+
+        public override bool Equals(object obj) {
+            if((obj == null) || this.GetType() != obj.GetType()) {
+                return false;
+            }
+
+            var symbolPlayer = (SymbolPlayer)obj;
+            return (this.symbol == symbolPlayer.symbol);
+        }
+
+        public bool Equals(SymbolPlayer symbolPlayer) {
+            return symbolPlayer.symbol == symbol;
         }
     }
 }
